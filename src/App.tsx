1e81db1b649908857cac79c0485e38a12de7ac0c
kimdonghyeon <kimdong799@gmail.com>
2024-03-15 23:54:05 +0900

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Text } from '@chakra-ui/react';
 import React from 'react';
 
 function App() {
-  return <div />;
+  return <div>
+    <Text color={"red.600"} fontSize={"6xl"}>It Works!</Text>
+  </div>;
 }
 
 export default App;
