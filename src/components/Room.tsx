c9c3a6659fd519a1ed99399e67f1b22b7f3a167b
kimdonghyeon <kimdong799@gmail.com>
2024-03-30 00:04:14 +0900

    Manual Fetching Rooms Get

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,29 +1,46 @@
 import { VStack, Box, Button, Grid, HStack, Image, Text, useColorModeValue } from "@chakra-ui/react";
 import { FaRegHeart, FaStar } from "react-icons/fa";
 
-export default function Room(){
+interface IRoomProps{
+    imageUrl:string;
+    name:string;
+    rating:number;
+    city:string;
+    country:string;
+    price:number;
+}
+
+export default function Room({
+    imageUrl, 
+    name, 
+    rating, 
+    city, 
+    country, 
+    price
+    }:IRoomProps){
     const textColor = useColorModeValue("gray.600", "gray.100");
     return(
         <VStack spacing={1} alignItems={"flex-start"}>
         <Box position={"relative"} overflow={"hidden"} rounded={"3xl"}>
             <Image 
             minH="280"
-            src="https://a0.muscache.com/im/pictures/miso/Hosting-49924321/original/cc3a98b7-d83e-4684-bb03-2b2ce6dd480d.jpeg?im_w=720" 
+            overflow={"clip"}
+            src={imageUrl}
             />
             <Button variant={"unstyled"} position={"absolute"} top={5} right={5} color={"white"}>
                 <FaRegHeart size="20px" />
             </Button>
         </Box>
         <Grid gap={2} templateColumns={"6fr 1fr"}>
-            <Text as={"b"} noOfLines={1} fontSize={"md"}>Namsan-myeon, Chuncheon, 한국의 집 전체ddsadasdsss</Text>
+            <Text as={"b"} noOfLines={1} fontSize={"md"}>{name}</Text>
             <HStack _hover={{color:"red.500"}} color={"gray"} spacing={1}>
                 <FaStar size={15}/>
-                <Text>5.0</Text>
+                <Text>{rating}</Text>
             </HStack>
         </Grid>
-        <Text fontSize={"sm"} color={textColor}>Seoul, S. Korea</Text>
+        <Text fontSize={"sm"} color={textColor}>{city}, {country}</Text>
         <Text fontSize={"sm"} color={textColor}>
-            <Text as={"b"}>$72</Text> / night
+            <Text as={"b"}>${price}</Text> / night
         </Text>
     </VStack>
     )
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,28 @@
-import { Box, Grid, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 import Room from "../components/Room";
 import { useEffect, useState } from "react";
 import RoomSkeleton from "../components/RoomSkeleton";
 
+interface IPhoto{
+    pk: string;
+    file: string;
+    description: string;
+}
+interface IRoom{
+    pk: number;
+    name: string;
+    country: string;
+    city: string;
+    price: number;
+    rating: number;
+    is_owner: boolean;
+    photos: IPhoto[];
+}
+
+
 export default function Home(){
     const [isLoading, setIsLoading] = useState(true);
-    const [rooms, setRooms] = useState();
+    const [rooms, setRooms] = useState<IRoom[]>([]);
     const fetchRooms = async() => {
         const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
         const json = await response.json();
@@ -31,23 +48,30 @@ export default function Home(){
             xl:"repeat(4, 1fr)",
             "2xl":"repeat(5, 1fr)"
         }}
-    >   
-            {isLoading ? (
-            <>
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            <RoomSkeleton />
-            </>
-            ) : null}
+    >
+      {isLoading ? (
+        <>
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+          <RoomSkeleton />
+        </>
+      ) : null}
+      {rooms.map(room => 
+        <Room
+            imageUrl={room.photos[0].file}
+            name={room.name}
+            rating={room.rating}
+            city={room.city}
+            country={room.country}
+            price={room.price}
+        />)}
     </Grid>
     )
 };
