fd82c9b1a5ce341e07e4d53cd3e0bccbd5d05a8f
kimdonghyeon <kimdong799@gmail.com>
2024-05-17 23:20:46 +0900

    Github Login

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -12,7 +12,13 @@ export default function SocialLogin() {
             <Divider />
         </HStack>
         <VStack>
-            <Button w={"100%"} leftIcon={<FaGithub />} colorScheme="telegram">
+            <Button 
+                as="a"
+                href="https://github.com/login/oauth/authorize?client_id=Ov23lizjBzgSJPSlxXl5&scope=read:user,user:email" 
+                w={"100%"} 
+                leftIcon={<FaGithub />} 
+                colorScheme="telegram"
+            >
                 Continue with Github
             </Button>
             <Button w={"100%"} leftIcon={<FaComment />} colorScheme="yellow">
