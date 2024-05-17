298a00e8852cc252c7fe694674464698e46cf6d3
kimdonghyeon <kimdong799@gmail.com>
2024-05-18 00:26:35 +0900

    Github Login Emails

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -31,3 +31,13 @@ export const logOut =  () =>
         "X-CSRFToken":Cookie.get("csrftoken") || "",
         }
     }).then(response => response.data);
+
+export const githubLogIn = (code:string) => instance.post(
+    `/users/github`,
+    { code },
+    {
+        headers: {
+            "X-CSRFToken": Cookie.get("csrftoken") || "",
+        },
+    }
+    ).then((response) => response.status);
